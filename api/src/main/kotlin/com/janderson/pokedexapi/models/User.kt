00f3fa2a3356c9d05ba35ae9c63432e1552ece92
Luiz Antonio <luizantonio_guimaraes98@hotmail.com>
2020-06-09 20:22:28 -0300

    adicionando classe usuario

--- api/src/main/kotlin/com/janderson/pokedexapi/models/User.kt
+++ api/src/main/kotlin/com/janderson/pokedexapi/models/User.kt
@@ -1,5 +1,18 @@
 package com.janderson.pokedexapi.models
 
-class User {
-    public var name: String = "Virto"
-}
+import javax.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long,
+
+        @Column(name = "name")
+        val name: String,
+        @Column (name = "email")
+        val email: String,
+        @Column (name = "password")
+        val password: String
+)
