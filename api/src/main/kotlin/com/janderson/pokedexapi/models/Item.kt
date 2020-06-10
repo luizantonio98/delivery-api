beb1690408680881da84d91f127ec02ede3a22d2
Luiz Antonio <luizantonio_guimaraes98@hotmail.com>
2020-06-09 21:00:04 -0300

    corrigindo bugs na entidade items

--- api/src/main/kotlin/com/janderson/pokedexapi/models/Item.kt
+++ api/src/main/kotlin/com/janderson/pokedexapi/models/Item.kt
@@ -1,12 +1,22 @@
 package com.janderson.pokedexapi.models
 
+import org.hibernate.annotations.GenericGenerator
 import javax.persistence.*
+import org.hibernate.annotations.Parameter
 
 @Entity
 @Table(name = "items")
 data class Item(
+        @Column(name = "id")
+        @GeneratedValue(generator = "itemSequenceGenerator")
+        @GenericGenerator(name = "itemSequenceGenerator",
+                strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+                parameters = [
+                    Parameter(name = "sequence_name", value = "seq_items_id"),
+                    Parameter(name = "initial_value", value = "1"),
+                    Parameter(name = "increment_size", value = "1")]
+        )
         @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long,
 
         @Column(name = "name")
@@ -14,5 +24,5 @@ data class Item(
 
         @ManyToOne
         @JoinColumn(name = "item_group_id")
-        var group : ItemGroup
+        var group: ItemGroup
 )
