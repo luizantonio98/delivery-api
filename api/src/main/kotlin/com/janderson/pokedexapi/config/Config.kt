489abc3a97baee2c22d1f66429a28d1520f6c3e3
JandersonCRB <jandersonangelo@hotmail.com>
2020-06-12 10:37:57 -0300

    Adding CORS

---
+++ api/src/main/kotlin/com/janderson/pokedexapi/config/Config.kt
@@ -0,0 +1,20 @@
+package com.janderson.pokedexapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class Config {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        }
+    }
+}
